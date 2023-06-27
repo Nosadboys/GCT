7144855bb3c4d22b2d6d4a17bd8494b399ad9074
Nosadboys <eld.kaz@mail.ru>
2023-06-27 13:02:15 +0300

    Del treash

--- js/theme_check.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const body = document.querySelector('body');
-    if(Cookies.get('theme')) {
-        const theme = Cookies.get('theme');
-        body.classList.add(theme);
-    }
-});
